10a5f4b06adf693fa6fb4639ece1b03efb99eaa9
Maria Jose Alonzo Claudino <mjaclaudino16@gmail.com>
2022-02-18 23:15:32 -0500

    updated

--- src/pages/OpenRoles.js
+++ src/pages/OpenRoles.js
@@ -5,6 +5,7 @@ import {  NavButton } from '../components/form-fields/buttons/NavButton';
 import { Footers } from '../layouts/footer/Footers';
 import { TopNav } from '../layouts/topNav/TopNav';
 import { Roles } from '../data/Roles';
+import { v4 as uuidv4 } from 'uuid';
 
 export const OpenRoles = () => {
     let navigate = useNavigate();
@@ -14,9 +15,6 @@ export const OpenRoles = () => {
     const openRole2Link = openRole2.replace(/\s/g, '+').toLowerCase();
     const openRole3 = 'Registered Nurse';
     const openRole3Link = openRole3.replace(/\s/g, '+').toLowerCase();
-
-    console.log(Roles)
-    console.log(Roles[1].title)
 
     return (
         <>
@@ -28,24 +26,25 @@ export const OpenRoles = () => {
                     <Collapsible accordion>
                         {Roles.map((role) => (
                             <CollapsibleItem
-                            expanded={false}
-                            header={role.title}
-                            node="div"
-                        >
+                                expanded={false}
+                                header={role.title}
+                                node="div"
+                                key={uuidv4()}
+                            >
                             <p className='subtitle'> Job Description: </p>
                             <p> {role.p1}
                             </p>
                             <p>{role.p2}</p>
                             <p>{role.p3}</p>
                             
-                            <p className='subtitle'> {role.p4} </p>
+                            <p className='subtitle'> Requirements: </p>
                             <p> {role.p5}</p>
                             <p> {role.p6}</p>
-                            <p className='subtitle'> {role.p7} </p>
+                            <p className='subtitle'> Job Type: </p>
                             <p> {role.p8}</p>
-                            <p className='subtitle'> {role.p9} </p>
+                            <p className='subtitle'> Salary: </p>
                             <p> {role.p10} </p>
-                            <p className='subtitle'> {role.p11} </p>
+                            <p className='subtitle'> Benefits: </p>
                             <p> {role.p12} </p>
                             <p> {role.p13} </p>
                             <p> {role.p14}</p>
